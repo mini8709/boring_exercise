0718f49f1788b64315f5f1c3a146cecccd20f6c3
mini8709 <33262600+mini8709@users.noreply.github.com>
2025-03-16 22:17:41 +0900

    feat: 스프링부트 + 코틀린 init (#3)

---
+++ src/main/kotlin/com/boring/BoringExerciseApplication.kt
@@ -0,0 +1,11 @@
+package com.boring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoringExerciseApplication
+
+fun main(args: Array<String>) {
+	runApplication<BoringExerciseApplication>(*args)
+}
